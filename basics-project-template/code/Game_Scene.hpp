1add5468d7aa380e736e9868b5f2c718b4628e8c
AlbertoEstirado <71897791+AlbertoEstirado@users.noreply.github.com>
2021-01-17 11:38:13 +0100

    game scene prototipe

--- basics-project-template/code/Game_Scene.hpp
+++ basics-project-template/code/Game_Scene.hpp
@@ -1,5 +1,5 @@
 /*
- * SAMPLE SCENE
+ * GAME SCENE
  * Copyright © 2020+ Alberto Estirado López
  *
  * Distributed under the Boost Software License, version  1.0
@@ -9,7 +9,58 @@
  */
 
 
+#include <memory>
+#include <basics/Scene>
+#include <basics/Texture_2D>
 
+namespace helicopter
+{
+    class Game_Scene : public basics::Scene
+    {
 
+        typedef std::shared_ptr< basics::Texture_2D > Texture_Handle;
+
+    public:
+
+        enum State
+        {
+            LOADING,
+            RUNNING,
+        };
+
+        State          state;
+        bool           suspended;
+
+        unsigned       canvas_width;
+        unsigned       canvas_height;
+
+        Texture_Handle texture;
+        float          x, y;
+
+    public:
+
+        Game_Scene();
+
+        basics::Size2u get_view_size () override
+        {
+            return { canvas_width, canvas_height };
+        }
+
+        bool initialize () override;
+        void suspend    () override;
+        void resume     () override;
+
+        void handle     (basics::Event & event) override;
+        void update     (float time) override;
+        void render     (basics::Graphics_Context::Accessor & context) override;
+
+    private:
+
+        void load ();
+        void run  (float time);
+
+    };
+
+}
 
 
